e9841aae32d0e15f89bc6ab0ed80793a4c3fe17e
Lippepaz <felipe.dapaz@hotmail.com>
2024-04-21 02:41:32 -0300

    feat:create login page

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,22 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoginResponse } from '../types/login-response.tyoe';
+import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  apiUrl: string = "http://localhost:8080/auth"
 
-  constructor (private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  login(name: string, password: string){
-    return this.httpClient.post<LoginResponse>("/login", { name, password }).pipe(
-      tap((value)=> {
-        sessionStorage.setItem("auth-Token", value.token)
+  login(email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
       })
     )
   }
 }
-3
